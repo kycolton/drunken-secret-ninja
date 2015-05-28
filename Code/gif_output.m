c9edb891c989ed0da093e66d76c8156a721a2eb8
Thomas Kwak <tkwak@volunteer.ucla.edu>
2015-05-27 22:10:32 -0700

    Added semicolons, fixed for loop dimension

--- Code/gif_output.m
+++ Code/gif_output.m
@@ -4,12 +4,12 @@ fname = 'indian_pines.gif';
 fps = 24;
 fh = figure(1);
 
-for i = 1:size(indian_pines_corrected)(3)
+for i = 1:size(indian_pines_corrected, 3)
     imagesc(indian_pines_corrected(:,:,i));
     drawnow;
-    f = getframe(fh)
-    im = frame2im(f)
-    [imind,cm] = rgb2ind(im,256)
+    f = getframe(fh);
+    im = frame2im(f);
+    [imind,cm] = rgb2ind(im,256);
     if i == 1;
         imwrite(imind,cm,fname,'gif','Loopcount',inf);
     else
