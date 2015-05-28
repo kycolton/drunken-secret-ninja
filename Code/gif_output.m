53245bbb894a4f45027216be43143b50f2b7b76d
Kyle Colton <ky.colton@gmail.com>
2015-05-27 19:25:38 -0700

    Modified to now write a .gif file. Unfortunately getframe isn't supported on Octave... Thomas, can you test this code?

--- Code/gif_output.m
+++ Code/gif_output.m
@@ -1,7 +1,19 @@
-% Iterates through the Indian Pines file and produces an image for each band
+% Iterates through the Indian Pines file and produces a gif for each band
+
+fname = 'indian_pines.gif';
+fps = 24;
+fh = figure(1);
 
 for i = 1:size(indian_pines_corrected)(3)
-	imagesc(indian_pines_corrected(:,:,i));
-	pause(1)
+    imagesc(indian_pines_corrected(:,:,i));
+    drawnow;
+    f = getframe(fh)
+    im = frame2im(f)
+    [imind,cm] = rgb2ind(im,256)
+    if i == 1;
+        imwrite(imind,cm,fname,'gif','Loopcount',inf);
+    else
+        imwrite(imind,cm,fname,'gif','WriteMode','append','DelayTime',1/fps)
+    end
 end
 
