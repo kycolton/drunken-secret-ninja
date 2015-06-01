13767be81c7cf84af9e0ec43c5480ac6e4f30bd2
Kyle Colton <ky.colton@gmail.com>
2015-06-01 12:24:36 -0700

    Fixed to iterate through and generate a plot at each frequency.

--- Code/km.R
+++ Code/km.R
@@ -1,11 +1,12 @@
 library(R.matlab)
 ip <- readMat('Data/Indian_pines_corrected.mat')
 ip <- array(unlist(ip),dim=c(145,145,200))
-#~ ip.long <- array(0,dim=c(dim(ip)[1] * dim(ip)[2], 3, dim(ip)[3]))
-#~ for( i in dim(ip)[3] )
-#~ {
-#~     ip.long[,,i] <- as.data.frame(melt(ip[,,i]))
-#~ }
-a <- as.data.frame(melt(ip[,,100]))
+ip.long <- array(0,dim=c(dim(ip)[1] * dim(ip)[2], 3, dim(ip)[3]))
+for( i in 1:(dim(ip)[3]) )
+{ ip.long[,,i] <- as.matrix(as.data.frame(melt(ip[,,i]))) }
 library(ggplot2)
-ggplot(a,aes(X1,X2,color=value))+geom_point()
+for( i in 1:200 )
+{
+    print(ggplot(as.data.frame(ip.long[,,i]),aes(V1,V2,color=V3))+geom_point())
+    readline("Press <return to continue") 
+}
