6e1e5ffdcf23b205087f10fc74c393512983705b
Thomas Kwak <tkwak@volunteer.ucla.edu>
2015-06-02 03:43:03 -0700

    added svm/other edits to presentation

--- Code/main.m
+++ Code/main.m
@@ -1,30 +1,57 @@
 % To be run from the ./Code/ directory
 load('../Data/Indian_pines_corrected.mat');
+load('../Data/Indian_pines_gt.mat');
 
 % Get sizes
 [n,m,k] = size(indian_pines_corrected);
 
 % Reshape with obs in each column
-obs = reshape(indian_pines_corrected,[k,n*m])';
+obs = reshape(indian_pines_corrected,[k,n*m]);
 
-% SVM SHOULD BE REWRITTEN FOR MORE CLASSES
+% All groundtruths are the same for each band
+test = indian_pines_gt(:, :, 1);
 
-% runs soft SVM for observations
-A = 2*randi([1,2],1,n*m)-3;
-[beta, c] = softsvm(obs.',A.',0.005);
+% All corn pixels have class 1, and everything else has class -1
+for i = 1:n
+    for j = 1:m
+        if(test(i, j) == 2 || test(i, j) == 3 || test(i, j) == 4)
+            test(i, j) = 1;
+        else
+            test(i, j) = -1;
+        end
+    end
+end
+test = reshape(test, [1, n*m]).';
 
-% plots softsvm
+% Run svm
+[beta, c] = softsvm(obs,test,0.005);
+
+%{
+% convert output back into map
+output = obs.' * beta + c;
+finalCalc = zeros(21025, 1);
+
+for i = 1:21025
+    if output(i) > -1
+        finalCalc(i) = 1;
+    else
+        finalCalc(i) = -1;
+    end
+end
+
+finalPic = reshape(finalCalc, [n, m]);
+imagesc(finalPic);
+%}
+
+% Plots softsvm
 fig = figure;
-plot(obs*beta+c, 'Color', 'red');
+plot(obs.'*beta+c, 'Color', 'red');
 hold on;
-legend('obs * beta + c', 'Location', 'southoutside');
+plot(-1, 'color', 'blue');
+hold on;
+xlabel('Data point') % x-axis label
+hold on;
+ylabel('Value of observation * beta + c') % y-axis label
 hold off;
 
-% saves figure as .png
 print(fig,'softsvmImage','-dpng')
-
-% runs k-means for 10 observations
-[C, labels] = km(obs.', 10);
-
-% save k-means data to csv
-csvwrite('labels.csv', labels);
